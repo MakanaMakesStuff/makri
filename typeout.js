f8b9f479f2893072a0a912d5a6bb2f6bfc14c594
MakanaMakesStuff <37227196+MakanaMakesStuff@users.noreply.github.com>
2019-06-01 01:46:46 -0400

    Typer.js
    This is a JavaScript plugin that takes the inner text of a given element, then outputs the text in a delayed fashion

---
+++ typeout.js
@@ -0,0 +1,67 @@
+/*
+#####################################################################################
+#####################################################################################
+##                                                                                 ##
+## Author: Makana O' Ke Akua Edwards                                               ##
+##                                                                                 ##
+## Date: 6/1/2019                                                                  ##
+##                                                                                 ##
+## Time: 1:08 am EST                                                               ##
+##                                                                                 ##
+## Description: This is a JavaScript plugin that takes the inner text of a given   ##
+## element, then outputs the text iteratively in a delayed fashion                 ##
+##                                                                                 ##
+## Copyright @ Makanaokeakua Edwards. This code can be used for any purpose.       ##
+## (If used in commercial projects, do not use in its original unaltered form).    ##
+##  All implementations of this code should cite myself as the original writer.    ##         ##                                                                                 ##
+#####################################################################################
+#####################################################################################
+*/
+function Typer(delay){
+    var types = document.querySelectorAll("[class*='type']");
+    if(this instanceof Typer){
+        var all = [...types];
+        this.typeout(all, delay);
+    }
+    else{
+        return new Typer(delay);
+    }
+}
+
+Typer.prototype.typeout = (initObj, delay)=>{
+    for(var i = 0; i < initObj.length; i++){
+        var right = initObj[i].className.split("-")[1];
+        if(/^(?=.*[a-zA-Z])(?=.*[0-9])/.test(right) || !/^[0-9]/.test(right)){
+            //Ignore and Let Loop Continue
+        }
+        else{
+            init(initObj[i]);
+        }
+    }
+    
+    function init (obj){
+        delay = parseInt(obj.className.split("-")[1]) || 1000;
+        var letters = obj.innerHTML;
+        obj.innerHTML = "";
+        var temp = "";
+        process(obj, letters, temp, 0, delay);
+    }
+    
+    function process (obj, letters, temp, count, delay){
+        if(count < letters.length){
+            temp += letters[count];
+            obj.innerHTML = temp;
+            count++;
+            setTimeout(function(){
+                return process(obj, letters, temp, count, delay);
+            }, delay);
+        }
+        else{
+            return;
+        }
+    }
+}
+
+window.onload = ()=>{
+    var typer = new Typer();
+}
